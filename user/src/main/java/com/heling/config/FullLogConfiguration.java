2b5c539f17f8c330638ba5cc5041e69e2fe76fd7
wanghleing <1915993172@qq.com>
2020-09-22 21:44:58 +0800

    update

---
+++ user/src/main/java/com/heling/config/FullLogConfiguration.java
@@ -0,0 +1,21 @@
+//package com.heling.config;
+//
+//import feign.Logger;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//
+///**
+// * @Author: heling
+// * @Date: 2020/9/22 21:29
+// * @Description: 设置feign的日志级别
+// */
+//@Configuration
+//public class FullLogConfiguration {
+//
+//    @Bean
+//    Logger.Level feignLoggerLevel() {
+//        return Logger.Level.FULL;
+//    }
+//
+//}
--- user/src/main/java/com/heling/service/OrderService.java
+++ user/src/main/java/com/heling/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.heling.service;
 
+//import com.heling.config.FullLogConfiguration;
 import com.heling.service.impl.OrderFallBackService;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
  * @Author: heling
  * @Date: 2020/9/22 16:19
  */
-@FeignClient(serviceId = "order-service",fallback = OrderFallBackService.class)
+@FeignClient(name = "order-service",
+        fallback = OrderFallBackService.class,
+        configuration = {})
 public interface OrderService {
 
     @GetMapping("order/order/{userId}")
