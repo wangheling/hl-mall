b881538d7128bcb26dd7c2ff611d1b375dca7292
wanghleing <1915993172@qq.com>
2020-09-22 22:50:42 +0800

    update

--- order/src/main/java/com/heling/service/impl/OrderServiceImpl.java
+++ order/src/main/java/com/heling/service/impl/OrderServiceImpl.java
@@ -4,6 +4,9 @@ import com.heling.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @Desc：
  * @Author: heling
@@ -14,6 +17,14 @@ import org.springframework.stereotype.Service;
 public class OrderServiceImpl implements OrderService {
     @Override
     public String test(String userId) {
+
+        long start = System.currentTimeMillis();
+        try {
+            TimeUnit.MILLISECONDS.sleep(new Random().nextInt(2000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("耗时{}ms", System.currentTimeMillis() - start);
         return "用户" + userId + "创建订单成功";
     }
 }
